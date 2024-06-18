908e7eb68185ba296cadcfc8622495095466f05b
lasinhmon <130166488+lasinhmon@users.noreply.github.com>
2024-06-18 20:49:45 +0700

    Update Connecting.cs

--- TNS.DBConnection/Connecting.cs
+++ TNS.DBConnection/Connecting.cs
@@ -2,11 +2,8 @@
 {
     public class Connecting
     {
-        //public static string SQL_MainDatabase = @"Data Source=WwoangFutt;DataBase=TN_Banking;user=sa;Password=123456;";
-        public static string SQL_MainDatabase = @"Data Source=171.235.24.157;DataBase=TN_Banking;user=KPI;Password=Tns6868#;";
-        //public static string SQL_MainDatabase = @"Data Source=TNS_Corporation\SQLEXPRESS;DataBase=TN_Banking;user=sa;Password=TriViet;";
-        //public static string SQL_KPI = @"Data Source=TNS_Corporation\SQLEXPRESS;DataBase=TN_KPI;user=sa;Password=TriViet;";
-        //public static string SQL_MainDatabase = @"Data Source=6280-WS000149;DataBase=TN_Banking;user=WebApp;Password=TriViet1234;";
-        //public static string SQL_KPI = @"Data Source=6280-WS000149;DataBase=TN_KPI;user=WebApp;Password=TriViet7808;";
+        
+        public static string SQL_MainDatabase = @"";
+  
     }
 } 
